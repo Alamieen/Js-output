0bf07570ab47d107296a6aaf83f5eafce3ea8cd0
Alamieen <88345348+Alamieen@users.noreply.github.com>
2022-06-24 02:08:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = 'Aminu Garba';
+let height = 1.7;
+let country = 'Nigeria'
+document.write('My name is' + ' ' + name)
+document.write(', I am ' + height + ' metres tall' )
+document.write(' and I am from ' + country)
